1cfac09fd7044e764ff0b9da621c4b14dbae1d9f
Roman Novikov <roman.olegovich.novikov@gmail.com>
2012-11-01 11:05:45 +0400

    add transaction created time

--- src/Gift/GeneralBundle/Entity/Transaction.php
+++ src/Gift/GeneralBundle/Entity/Transaction.php
@@ -220,4 +220,54 @@ class Transaction
     {
         return $this->debug;
     }
+    /**
+     * @var \DateTime $created_at
+     */
+    private $created_at;
+
+
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return Transaction
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = new \DateTime;
+    
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+    /**
+     * @ORM\PrePersist
+     */
+    public function processPrePersist() {
+        $this->setCreatedAt();
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function processPostPersist()
+    {
+        // Add your code here
+    }
+
+    /**
+     * @ORM\PostUpdate
+     */
+    public function processPostUpdate()
+    {
+        // Add your code here
+    }
 }
